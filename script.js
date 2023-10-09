0b7f4275f37e62f991c0d924b7861f5d5c122506
shivam-sharma-03 <shivamsharma2022@jklu.edu.in>
2023-10-09 21:45:15 +0530

    redesigned button

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
 // Initialize an empty string to store the user input
-let inputString = ''
+let inputString = '';
 
 // Initialize the variable to store the current memory value to zero
-let memoryValue = 0
+let memoryValue = 0;
 
 // Select all the calculator buttons on the page
-const calculatorButtons = document.querySelectorAll('.button')
+const calculatorButtons = document.querySelectorAll('button');
 
 // Add an event listener to each button
 calculatorButtons.forEach(button => {
   button.addEventListener('click', () => {
-    // Check if the clicked button is the equals sign
-    if (button.textContent === '=') {
-      // Evaluate the user input as a mathematical expression
-      inputString = eval(inputString)
+    const buttonValue = button.textContent;
 
-      // Display the result in the input field
-      document.querySelector('input').value = inputString
+    switch (buttonValue) {
+      case '=':
+        // Check if the inputString is not empty and contains a valid expression
+        if (inputString) {
+          try {
+            // Replace percentage symbol (%) with '/100' before evaluation
+            inputString = inputString.replace(/%/g, '/100');
+            
+            // Evaluate the user input as a mathematical expression
+            const result = eval(inputString);
 
-    // Check if the clicked button is the clear button
-    } else if (button.textContent === 'C') {
-      // Reset the user input string and the displayed input field
-      inputString = ''
-      document.querySelector('input').value = inputString
+            // Display the result in the input field
+            document.querySelector('input').value = result;
 
-    // Check if the clicked button is the memory plus button
-    } else if (button.textContent === 'M+') {
-      // Add the current input value to the memory value
-      memoryValue += parseFloat(inputString)
+            // Update the inputString with the result for further calculations
+            inputString = result.toString();
+          } catch (error) {
+            // Handle invalid expressions
+            document.querySelector('input').value = 'Error';
+            inputString = '';
+          }
+        }
+        break;
+      case 'C':
+        // Clear the inputString and reset the input field
+        inputString = '';
+        document.querySelector('input').value = inputString;
+        break;
+      case 'M+':
+        // Add the current input value to the memory value
+        memoryValue += parseFloat(inputString) || 0;
+        inputString = '';
+        break;
+      case 'M-':
+        // Subtract the current input value from the memory value
+        memoryValue -= parseFloat(inputString) || 0;
+        inputString = '';
+        break;
+      default:
+        // Append the clicked button's value to the input string
+        if (buttonValue === '%') {
+          // If it's a percentage, divide the current input value by 100
+          inputString = (parseFloat(inputString) / 100).toString();
+        } else {
+          inputString += buttonValue;
+        }
 
-      // Reset the input string
-      inputString = ''
-
-    // Check if the clicked button is the memory minus button
-    } else if (button.textContent === 'M-') {
-      // Subtract the current input value from the memory value
-      memoryValue -= parseFloat(inputString)
-
-      // Reset the input string
-      inputString = ''
-
-    // Check if the clicked button is the memory clear button
-    } else if (button.textContent === 'MC') {
-      // Reset the memory value to zero
-      memoryValue = 0
-
-    // If none of the above buttons were clicked, append the button's value to the input string
-    } else {
-      // Append the clicked button's value to the input string
-      inputString += button.textContent
-
-      // Display the updated input string in the input field
-      document.querySelector('input').value = inputString
+        // Display the updated input string in the input field
+        document.querySelector('input').value = inputString;
+        break;
     }
-  })
-})
+  });
+});
